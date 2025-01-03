35c9feabc0d744d5c65ba9d862c20c065d1e4759
Seedling <david.r.graham32@gmail.com>
2025-01-03 15:37:53 -0500

    Create home, customers and tickets pages Create simple landing page with business card and background

---
+++ src/app/(rs)/tickets/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const tickets = () => {
+  return (
+    <div>tickets page</div>
+  )
+}
+
+export default tickets
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
   	extend: {
+      backgroundImage: {
+        'home-img': "url('/images/home-img.jpg')",
+      },
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -58,5 +61,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  // plugins: [require("tailwindcss-animate")],
 } satisfies Config;
